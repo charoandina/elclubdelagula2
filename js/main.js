da69565fa8c712229438a98ed072fda089324deb
Rosario Garcia de Andina <rosario.garciadea@gmail.com>
2023-02-09 16:09:05 +0100

    agrego css, avances en js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const nombreUsuario = prompt ("Ingrese su nombre");
+/* const nombreUsuario = prompt ("Ingrese su nombre");
 const edadUsuario = prompt ("Ingrese su edad");
 
 if (edadUsuario < "18") {
@@ -62,3 +62,110 @@ if (bebidaUsuario == "si") {
 }
 
 alert ("Gracias, vuelva prontos");
+ */
+
+// FUNCTION
+
+/* function solicitarEdad () {
+    const nombreUsuario = prompt ("Ingrese su nombre");
+    const edadUsuario = prompt ("Ingrese su edad");
+
+    if (edadUsuario < "18") {
+    alert ("Lo siento, " + nombreUsuario + " no tenes la edad suficiente para ingresar al Club de la Gula");
+    } else {
+    alert ("Hola " + nombreUsuario + "! Tenes " + edadUsuario + " años, bienvenido al Club de la Gula!");
+    } 
+}
+
+solicitarEdad (); */
+
+class Producto {
+    constructor (nombre, precio, descripcion) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+}
+
+const producto1 = new Producto("Milka Oreo", 300, "Un clasico mas otro clasico que no fallan nunca");
+const producto2 = new Producto("Milka Chips Ahoy", 300, "Te sorpendimos con este, probalo que es una bomba");
+const producto3 = new Producto("Milka con leche", 250, "El original, ese que comía la abuela");
+const producto4 = new Producto("Milka Caramel", 320, "Un golazo de media cancha");
+const producto5 = new Producto("Kinder Bueno", 435, "No confies en nadie que no le guste");
+const producto6 = new Producto("Kinder Bueno Blanco", 450, "El original pero blanco, ¿qué más podes pedir?");
+const producto7 = new Producto("Kit-Kat", 220, "Barritas del bien que sanan el corazón");
+
+const productos =  [producto1, producto2, producto3, producto4, producto5, producto6, producto7];
+
+function mostrarProductos() {
+    alert ("Estos productos tenemos en venta hoy:")
+    for (const producto of productos) {
+    alert (`Nombre: ${producto.nombre}, $${producto.precio}. ${producto.descripcion}`);
+}
+}
+
+mostrarProductos ();
+
+const productoUsuario = prompt ("Ingresa el nombre del producto que quieras");
+const cantidadProducto = parseInt (prompt ("¿Cuantos vas a querer?"));
+
+
+if (productoUsuario == "Milka Oreo") {
+    let subtotal = cantidadProducto * producto1.precio
+    console.log(subtotal)
+} else (productoUsuario)
+
+switch (productoUsuario) {
+    case "Milka Oreo":
+    case "milka oreo":
+        let subtotal = cantidadProducto * producto1.precio
+        alert ("Tu pedido es: " + productoUsuario + ", y vas a querer: " + cantidadProducto + ". Será un total de: $" + subtotal )
+        break;
+    case "Milka Chips Ahoy":
+    case "milka chips ahoy":
+        let subtotal2 = cantidadProducto * producto2.precio
+        alert ("Tu pedido es: " + productoUsuario + ", y vas a querer: " + cantidadProducto + ". Será un total de: $" + subtotal )
+        break;
+    default:
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+//ARRAY
+
+/* const productosEnVenta = [
+    {nombre: },
+    {nombre: },
+    {nombre: },
+    {nombre: },
+    {nombre: },
+    {nombre: },
+    {nombre: },
+]
+
+/* function mostrarProductos() {
+    alert ("Estos productos tenemos en venta hoy:")
+    for (const producto of productosEnVenta) {
+        alert ((`Nombre: ${producto.nombre}`) + (`, $${producto.precio}`) + (`. ${producto.descripcion}`));
+    } 
+    alert ("¿Ya decidiste que vas a pedir?")
+}
+
+mostrarProductos (); */
+/* console.log(productosEnVenta)
+
+const eleccionCliente = prompt ("Escribí el nombre del producto que quieras")
+
+if (eleccionCliente == "Milka Oreo") {
+    const cantidadCliente = parseInt (prompt("¿Cuantos vas a querer?"));
+    const subtotal = 
+} */
